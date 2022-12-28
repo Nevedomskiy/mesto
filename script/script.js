5efedb747d131bdd1538c7af889dd3f27de68ab5
Nevedomskiy <nevedomsky.vasily@yandex.ru>
2022-12-28 11:38:42 +0300

    fix: BEM modifier name button in profile, style:edit position elements with Pixel Perfect, fix: JS function

--- script/script.js
+++ script/script.js
@@ -1,14 +1,19 @@
 let content = document.querySelector('.content');
 let profile = content.querySelector('.profile');
-let buttonEdit = profile.querySelector('.button_edit');
-let buttonAdd = profile.querySelector('.button_add');
+let buttonEdit = profile.querySelector('.profile__button-edit');
+let buttonAdd = profile.querySelector('.profile__button-add');
 let popup = document.querySelector('.popup');
 let popupContainer = popup.querySelector('.popup__container');
 let popupSubmitBtn = popup.querySelector('.popup__submit-btn');
 let popupClose = popup.querySelector('.popup__close');
+let name = popupContainer.querySelector('.popup__text_type_name');
+let profession = popupContainer.querySelector('.popup__text_type_profession');
+let form = document.forms.profileForm;
 
 function openPopap() {
    popup.classList.add('popup_opened');
+   name.value = document.querySelector('.profile__name').textContent;
+   profession.value = document.querySelector('.profile__subname').textContent;
 }
 
 function closePopap() {
@@ -17,17 +22,11 @@ function closePopap() {
 
 function changeProfile(evt) {
    evt.preventDefault();
-
-   let name = popupContainer.querySelector('.popup__text_type_name').value;
-   document.querySelector('.profile__name').textContent = name;
-
-   let profession = popupContainer.querySelector('.popup__text_type_profession').value;
-   document.querySelector('.profile__subname').textContent = profession;
+   document.querySelector('.profile__name').textContent = name.value;
+   document.querySelector('.profile__subname').textContent = profession.value;
    closePopap();
 }
 
-popupSubmitBtn.addEventListener('click', changeProfile);
+form.addEventListener('submit', changeProfile);
 buttonEdit.addEventListener('click', openPopap);
 popupClose.addEventListener('click', closePopap);
-
-
